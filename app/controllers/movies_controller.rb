accc382366bdc071802928cfca52da0cce8bf2f0
Artur Krol <kroolar@gmail.com>
2019-10-24 12:30:49 +0200

    production test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,11 +5,32 @@ class MoviesController < ApplicationController
     def new
         @movie = Movie.new
     end
+
+    def show
+        @movie = Movie.find(params[:id])
+    end
+
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+        @movie.update(movie_params)
+        redirect_to movies_path
+    end
+
     def create
         @movie = Movie.new(movie_params)
         @movie.save
         redirect_to movies_path
     end
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_path
+    end
+    private
     def movie_params
         params.require(:movie).permit(:title, :year, :rate)
     end
